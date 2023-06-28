53585719e9ae2d567fec439d1a1fd557e36d1352
aniindyta <aninditaputri055@gmail.com>
2023-06-28 12:24:27 +0700

    tes

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float moveSpeed = 5f;
+    public Transform movePoint;
+    public LayerMask noMove;
+
+    private void Start()
+    {
+        movePoint.parent = null;
+    }
+
+    private void Update()
+    {
+        transform.position = Vector3.MoveTowards(transform.position, movePoint.position, moveSpeed * Time.deltaTime);
+
+        if (Vector3.Distance(transform.position, movePoint.position) <= 0.05f) {
+            if(Mathf.Abs(Input.GetAxisRaw("Horizontal")) == 1f) {
+                if(!Physics2D.OverlapCircle(movePoint.position + new Vector3(Input.GetAxisRaw("Horizontal"), 0f, 0f), 0.2f, noMove)) {
+                    movePoint.position += new Vector3(Input.GetAxisRaw("Horizontal"), 0f, 0f);
+                }
+            } else if(Mathf.Abs(Input.GetAxisRaw("Vertical")) == 1f) {
+                if(!Physics2D.OverlapCircle(movePoint.position + new Vector3(0f, Input.GetAxisRaw("Vertical"), 0f), 0.2f, noMove)) {
+                    movePoint.position += new Vector3(0f, Input.GetAxisRaw("Vertical"), 0f);
+                }
+            }
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Enemy") {
+            Debug.Log("Enemy");
+            Time.timeScale = 0;
+        }
+
+        if (collision.gameObject.tag == "Goal") {
+            Debug.Log("Goal");
+            Time.timeScale = 0;
+        }
+    }
+}
